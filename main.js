eabe94be6a481ede95158898a33c81516c2651cb
apotecario1277 <lucasbrancalhao454@gmail.com>
2020-02-03 16:27:58 -0300

    conexão com o xml

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-try{
-  var xml = new XMLHttpRequest();
+  const xml = new XMLHttpRequest();
+  console.log(xml);
   xml.open("GET", "https://rss.tecmundo.com.br/feed");
+  xml.addEventListener("load", function(){
+    var resp = xml.responseText;
+    console.log(resp);
+  });
   xml.send();
-  console.log(xml)
-
-}catch(e){
-  xml.abort();
-  console.log("Error in " + e)
-}
