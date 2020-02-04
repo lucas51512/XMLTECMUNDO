e97c38e488ca2b09bd4029e0a688ddb160a4ac7a
apotecario1277 <lucasbrancalhao454@gmail.com>
2020-02-04 14:36:24 -0300

    Adicionado o parser e elementos como: getElementByTagName

--- main.js
+++ main.js
@@ -3,6 +3,15 @@
   xml.open("GET", "https://rss.tecmundo.com.br/feed");
   xml.addEventListener("load", function(){
     var resp = xml.responseText;
-    console.log(resp);
+
   });
   xml.send();
+
+  var parser = new DOMParser();
+  var xmlDoc = parser.parseFromString(xml, "text/xml");
+  console.log(xmlDoc);
+
+  document.getElementsByTagName('image').innerHTML = xmlDoc.getElementsByTagName('image')[0];
+  var title = xmlDoc.getElementsByTagName('title')[0];
+  var description = xmlDoc.getElementsByTagName('description')[0];
+  console.log(img);
