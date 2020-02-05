3b4680e224dcace7a43cb1803a8dcc9d64a99852
apotecario1277 <lucasbrancalhao454@gmail.com>
2020-02-05 14:55:05 -0300

    main.js com registro, registro com erro

--- js/main.js
+++ js/main.js
@@ -31,13 +31,10 @@
   xml.send();
 
   if ('serviceWorker' in navigator) {
-window.addEventListener('load', function() {
-  navigator.serviceWorker.register('/sw.js').then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-  }, function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
-});
+window.addEventListener('load', () => {
+  navigator.serviceWorker
+  .register("serviceWorker.js")
+  .then(reg => console.log("serviceWorker: Registered"))
+  .catch(err => console.log("serviceWorker: Error" + err) )
+})
 }
