2d0af052fe4c2714f23d4f67a6ce82cd9de47ec7
apotecario1277 <lucasbrancalhao454@gmail.com>
2020-02-05 13:50:02 -0300

    nova pasta js e inicio com o service worker

---
+++ js/main.js
@@ -0,0 +1,43 @@
+  const xml = new XMLHttpRequest();
+  var resp = '';
+  xml.open("GET", "https://rss.tecmundo.com.br/feed");
+  xml.addEventListener("load", function(){
+    resp = xml.responseText;
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(resp, "text/xml");
+
+    const  img = xmlDoc.getElementsByTagName("image")[0];
+    const  url = img.getElementsByTagName("url")[0];
+    const  title = xmlDoc.getElementsByTagName("title")[0];
+    const  description = xmlDoc.getElementsByTagName("description")[0];
+
+    var div = document.createElement("div")
+    var image = document.createElement("img");
+    var h1 = document.createElement("h1");
+    var p = document.createElement("p");
+    image.src = url.textContent;
+
+    document.body.appendChild(div);
+    div.appendChild(image);
+    div.appendChild(h1);
+    div.appendChild(p);
+
+    h1.textContent = title.textContent;
+    p.textContent = description.textContent;
+
+
+
+  });
+  xml.send();
+
+  if ('serviceWorker' in navigator) {
+window.addEventListener('load', function() {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }, function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+});
+}
