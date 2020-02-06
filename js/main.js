5e110f9b1e637bd1d45fa88bd4834dfe4d7d1ff2
apotecario1277 <lucasbrancalhao454@gmail.com>
2020-02-06 13:34:30 -0300

    novo arquivo css

--- js/main.js
+++ js/main.js
@@ -6,26 +6,28 @@
     var parser = new DOMParser();
     var xmlDoc = parser.parseFromString(resp, "text/xml");
 
-    const  img = xmlDoc.getElementsByTagName("image")[0];
-    const  url = img.getElementsByTagName("url")[0];
-    const  title = xmlDoc.getElementsByTagName("title")[0];
-    const  description = xmlDoc.getElementsByTagName("description")[0];
+for (var i = 0; i < xmlDoc.length; i++) {
+  const  img = xmlDoc.getElementsByTagName("image")[0];
+  const  url = img.getElementsByTagName("url")[0];
+  const  title = xmlDoc.getElementsByTagName("title")[0];
+  const  description = xmlDoc.getElementsByTagName("description")[0];
 
-    var div = document.createElement("div")
-    var image = document.createElement("img");
-    var h1 = document.createElement("h1");
-    var p = document.createElement("p");
-    image.src = url.textContent;
+  var div = document.createElement("div")
+  var image = document.createElement("img");
+  var h1 = document.createElement("h1");
+  var p = document.createElement("p");
+  image.src = url.textContent;
 
-    document.body.appendChild(div);
-    div.appendChild(image);
-    div.appendChild(h1);
-    div.appendChild(p);
+  document.body.appendChild(div);
+  div.appendChild(image);
+  div.appendChild(h1);
+  div.appendChild(p);
 
-    h1.textContent = title.textContent;
-    p.textContent = description.textContent;
+  h1.textContent = title.textContent;
+  p.textContent = description.textContent;
 
 
+}
 
   });
   xml.send();
